0e5c1848f1bc04fe5c3ec708c06d9d5ea9b8d811
patryk <random12@onet.pl>
2025-12-14 21:03:38 +0100

    first commit

---
+++ random_forest_regression/prepare_data.py
@@ -0,0 +1,14 @@
+
+YEARS_WEATHER = [2020,2021,2022,2023,2024]
+IMGW_STATION_ID = 566
+GIOS_STATION_CODE = 'MpKrakBulwar'
+
+FILES_PM10 = ["2020_PM10_1g.xlsx","2021_PM10_1g.xlsx","2022_PM10_1g.xlsx", "2023_PM10_1g.xlsx", "2024_PM10_1g.xlsx"]
+FILES_PM25 = ["2020_PM25_1g.xlsx", "2021_PM25_1g.xlsx", "2022_PM25_1g.xlsx", "2023_PM25_1g.xlsx", "2024_PM25_1g.xlsx"]
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/120.0.0.0 Safari/537.36'
+}
+
+def fetch_imgw_data(years):
+    pass
